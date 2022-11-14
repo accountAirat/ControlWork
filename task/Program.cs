2eccb6a5e35fa9a2deaa07bfd9a570aa552be7a6
Airat <mingaleev.1993@mail.ru>
2022-11-14 21:38:46 +0300

    Дописали программу

--- task/Program.cs
+++ task/Program.cs
@@ -4,16 +4,27 @@
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i].Length <= standard)
-        {
-            Console.WriteLine(arr[i] + " " + arr[i].Length);
             count++;
-        }
     }
     return count;
 }
-
+void FillNewArray(string[] arr, string[] newArr, int standard)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= standard)
+        {
+            newArr[count] = arr[i];
+            count++;
+        }
+    }
+}
 Console.Clear();
 string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
 int standardSize = 3;
 Console.WriteLine(string.Join(", ", array));
-Console.Write(ElementCounting(array, standardSize));
+ElementCounting(array, standardSize);
+string[] newArray = new string[ElementCounting(array, standardSize)];
+FillNewArray(array, newArray, standardSize);
+Console.WriteLine(string.Join(", ", newArray));
