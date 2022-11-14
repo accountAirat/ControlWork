bb0d07bbf0ae4309db209d0353975d06a0388c2b
Airat <mingaleev.1993@mail.ru>
2022-11-14 21:30:30 +0300

    Написал функцию подсчёта

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,19 @@
-﻿string[]array = ["hello", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"];
+﻿int ElementCounting(string[] arr, int standard)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= standard)
+        {
+            Console.WriteLine(arr[i] + " " + arr[i].Length);
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+int standardSize = 3;
+Console.WriteLine(string.Join(", ", array));
+Console.Write(ElementCounting(array, standardSize));
